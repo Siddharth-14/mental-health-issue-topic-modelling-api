01f8ac306e6705956fe259cc00b6f1095ce59c02
siddharth <srg6@sfu.ca>
2023-05-21 18:49:33 -0700

    updated

--- functions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .app import create_app
-
-# Create the Flask app object using the create_app() function
-app = create_app()
--- serverless.py
+++
@@ -1,5 +0,0 @@
-from functions.app import create_app
-
-def handler(event, context):
-    app = create_app()
-    return app(event, context)
